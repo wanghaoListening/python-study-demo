e72106e69c98e61768717e9a08c60375d1de4f7c
wanghaoListening <wanghaotime@qq.com>
2018-02-11 14:11:34 +0800

    network tcp

---
+++ third-party-module/use_virtualenv.py
@@ -0,0 +1,9 @@
+
+'''
+在开发Python应用程序的时候，系统安装的Python3只有一个版本：3.4。所有第三方的包都会被pip安装到Python3的site-packages目录下。
+如果我们要同时开发多个应用程序，那这些应用程序都会共用一个Python，就是安装在系统的Python 3。如果应用A需要jinja 2.7，而应用B需要jinja 2.6怎么办？
+这种情况下，每个应用可能需要各自拥有一套“独立”的Python运行环境。virtualenv就是用来为一个应用创建一套“隔离”的Python运行环境
+
+pip3 install virtualenv
+'''
+
