0118221e3782960a22a56576f3f58bc5994f56df
wanghaoListening <wanghaotime@qq.com>
2018-02-28 11:09:45 +0800

    爬虫脚本锻炼

---
+++ python-scraping-master/chapter3/2-crawlWikipedia.py
@@ -0,0 +1,28 @@
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+pages = set()
+
+def getLinks(pageUrl):
+    global pages
+    html = urlopen("http://en.wikipedia.org"+pageUrl)
+    bsObj = BeautifulSoup(html,"html.parser")
+    try:
+        print(bsObj.h1.get_text())
+        print(bsObj.find(id="mw-content-text").findAll("p")[0])
+        print(bsObj.find(id="ca-edit").find("span").find("a").attrs['href'])
+    except AttributeError as e:
+        print(e)
+
+    for link in bsObj.findAll("a", href=re.compile("^(/wiki/)")):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                newPage = link.attrs['href']
+                print("------------------\n"+newPage)
+                pages.add(newPage)
+                getLinks(newPage)
+
+
+getLinks("")
