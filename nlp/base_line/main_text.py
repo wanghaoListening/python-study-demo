c5a3684708660275f5d2a41cc0ea608ddfd2f1cd
hiwanghao <hiwanghao@didiglobal.com>
2021-05-07 19:43:50 +0800

    add NLP中的对抗训练 + PyTorch实现

---
+++ nlp/base_line/main_text.py
@@ -0,0 +1,39 @@
+from .fgm import FGM
+from .pgd import PGD
+
+fgm = FGM()
+for batch_input, batch_label in data:
+    # 正常训练
+    loss = model(batch_input, batch_label)
+    loss.backward() # 反向传播，得到正常的grad
+    # 对抗训练
+    fgm.attack() # 在embedding上添加对抗扰动
+    loss_adv = model(batch_input, batch_label)
+    loss_adv.backward() # 反向传播，并在正常的grad基础上，累加对抗训练的梯度
+    fgm.restore() # 恢复embedding参数
+    # 梯度下降，更新参数
+    optimizer.step()
+    model.zero_grad()
+
+
+pgd = PGD()
+K = 3
+for batch_input, batch_label in data:
+    # 正常训练
+    loss = model(batch_input, batch_label)
+    loss.backward() # 反向传播，得到正常的grad
+    pgd.backup_grad()
+    # 对抗训练
+    for t in range(K):
+        pgd.attack(is_first_attack=(t==0)) # 在embedding上添加对抗扰动, first attack时备份param.data
+        if t != K-1:
+            model.zero_grad()
+        else:
+            pgd.restore_grad()
+        loss_adv = model(batch_input, batch_label)
+        loss_adv.backward() # 反向传播，并在正常的grad基础上，累加对抗训练的梯度
+    pgd.restore() # 恢复embedding参数
+    # 梯度下降，更新参数
+    optimizer.step()
+    model.zero_grad()
+
